4f4f4acd6512446c8406b2b0fbe846d461dbf012
13170212 <13170274@itculiacan.edu.mx>
2019-04-29 13:57:14 -0600

    base data

---
+++ crud_node/src/app.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const myConnection = require('express-myconnection');
+const mysql = require('mysql');
+
+//importar rutas
+const indiceRutas=require('./rutas/index');
+
+//usamos las rutas
+app.use('/',indiceRutas);
+
+//configuraciones
+app.set('port',process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'views'));
+
+
+//MiddleWare (Morgan)
+//app.use(morgan('dev')); //dev = desarrollo
+
+//middleware
+
+app.use(myConnection(mysql,
+{
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	port: 3306,
+	database: 'crudnodejsmysql13'
+},'single'));
+
+
+//Activar el servidor de escucha
+app.listen(3000, () => {
+	console.log("Escuchando en puerto 3000, jajaja");
+}
+)
